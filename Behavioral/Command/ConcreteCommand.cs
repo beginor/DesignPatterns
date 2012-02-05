2bdc1c82302db05413d59fd55df1b3346f29ff0d
beginor <beginor@qq.com>
2012-02-06 07:31:07 +0800

    add  file

---
+++ Behavioral/Command/ConcreteCommand.cs
@@ -0,0 +1,17 @@
+﻿namespace Command {
+
+	public class ConcreteCommand : ICommand {
+
+		private readonly Receiver _receiver;
+		private object _state;
+
+		public ConcreteCommand(Receiver receiver) {
+			this._receiver = receiver;
+		}
+
+		public void Execute() {
+			this._receiver.Action();
+		}
+
+	}
+}
---
+++ Behavioral/Command/Receiver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Command {
+	
+	public class Receiver {
+		
+		public void Action() {
+			Console.WriteLine("Receiver Action.");
+		}
+
+	}
+}
