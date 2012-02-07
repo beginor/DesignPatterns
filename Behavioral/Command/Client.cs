ced3771064f714fc9f64ed647e8fa9d3113d04f9
beginor <beginor@qq.com>
2012-02-07 21:18:52 +0800

    implement command pattern

---
+++ Behavioral/Command/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Command {
+
+	class Client {
+		
+		static void Main(string[] args) {
+
+			var invoker = new Invoker();
+
+			var receiver = new Receiver();
+			ICommand command = new ConcreteCommand(receiver);
+			
+			invoker.InvokeCommand(command);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Behavioral/Command/Invoker.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Command {
+
+	public class Invoker {
+		
+		private readonly IList<ICommand> _commandHistory = new List<ICommand>();
+
+		public void InvokeCommand(ICommand command) {
+			command.Execute();
+			this._commandHistory.Add(command);
+		}
+	}
+}
