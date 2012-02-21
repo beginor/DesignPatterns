54b57fec4e469413673b2d8c81b1df4fed666507
beginor <beginor@qq.com>
2012-02-21 20:33:20 +0800

    add Momento pattern

---
+++ Behavioral/Memento/CareTaker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Memento {
+	
+	class CareTaker {
+
+		static void Main(string[] args) {
+			IList<Originator.Momento> savedStates = new List<Originator.Momento>();
+
+			var originator = new Originator();
+			originator.SetState("State1");
+			originator.SetState("State2");
+			savedStates.Add(originator.SaveToMomento());
+			originator.SetState("State3");
+			savedStates.Add(originator.SaveToMomento());
+			originator.SetState("State4");
+			originator.RestoreFromMomento(savedStates[0]);
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Behavioral/Memento/Originator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Memento {
+
+	public class Originator {
+
+		private string _state;
+
+		public void SetState(string state) {
+			Console.WriteLine("Originator: setting momento to {0}", state);
+			this._state = state;
+		}
+
+		public Momento SaveToMomento() {
+			Console.WriteLine("Originator: saving to momento...");
+			return new Originator.Momento(this._state);
+		}
+
+		public void RestoreFromMomento(Momento momento) {
+			this._state = momento.State;
+			Console.WriteLine("Originator: resotring from momento {0}", this._state);
+		}
+		
+		public class Momento {
+
+			private readonly string _state;
+
+			internal Momento(string state) {
+				this._state = state;
+			}
+
+			public string State {
+				get {
+					return this._state;
+				}
+			}
+		}
+	}
+}
