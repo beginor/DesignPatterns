24092d26562534e28d7ef5c3acb4a4318c3bbfed
beginor <beginor@qq.com>
2012-02-28 07:20:08 +0800

    add singleton pattern

---
+++ Creational/Singleton/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Singleton {
+
+	public class DoublecheckSingleton {
+
+		private volatile static DoublecheckSingleton _instance;
+
+		private DoublecheckSingleton() {
+		}
+
+		public static DoublecheckSingleton Instance {
+			get {
+				if (_instance == null) {
+					lock (typeof(DoublecheckSingleton)) {
+						if (_instance == null) {
+							Console.WriteLine("Initialize a double check singleton.");
+							_instance = new DoublecheckSingleton();
+						}
+					}
+				}
+				return _instance;
+			}
+		}
+	}
+
+	public class LazySingleton {
+
+		private static readonly Lazy<LazySingleton> LazyInstance = new Lazy<LazySingleton>(() => {
+			Console.WriteLine("Initialize a lazy singleton.");
+			return new LazySingleton();
+		});
+
+		private LazySingleton() {
+		}
+
+		public static LazySingleton Instance {
+			get {
+				return LazyInstance.Value;
+			}
+		}
+	}
+
+	class Program {
+
+		static void Main(string[] args) {
+
+			for (int i = 0; i < 100; i++) {
+				Task.Factory.StartNew(() => Console.WriteLine(DoublecheckSingleton.Instance.ToString()));
+			}
+
+			for (int i = 0; i < 100; i++) {
+				Task.Factory.StartNew(() => Console.WriteLine(LazySingleton.Instance.ToString()));
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Creational/Singleton/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Singleton")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Singleton")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d14f4089-6605-4004-8400-853b36efe937")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
